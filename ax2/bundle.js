6ada26c25158a162a27a6fc289d4cfb6431983dd
Allan-Araujo <Allan.Araujo@coxautoinc.com>
2019-12-26 17:48:11 -0300

    Ex3 starting

---
+++ ax2/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var arr = [1, 3, 4, 5, 8, 9];
+var newArr = arr.map(function (item, index) {
+  return total + index;
+});
+console.log(newArr);
---
+++ ax2/main.js
@@ -0,0 +1,7 @@
+const arr = [1,3,4,5,8,9];
+
+const newArr = arr.map(function(item, index){
+        return total + index;
+})
+
+console.log(newArr);
